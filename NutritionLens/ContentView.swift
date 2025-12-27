009bdf5b40d409b593c47e83bd7af2d9b163f199
patricianandres <ppia4382@gmail.com>
2025-12-27 17:11:49 +0900

    Initial Commit

---
+++ NutritionLens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NutritionLens
+//
+//  Created by Patrician Andres on 2025/12/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
