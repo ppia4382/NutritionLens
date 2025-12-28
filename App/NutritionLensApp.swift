044bf6ac396cb286f2587454915686e5f507c2b3
patricianandres <ppia4382@gmail.com>
2025-12-28 12:31:47 +0900

    chore: organize existing files into App, Presentation, and Resources modules

---
+++ App/NutritionLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutritionLensApp.swift
+//  NutritionLens
+//
+//  Created by Patrician Andres on 2025/12/27.
+//
+
+import SwiftUI
+
+@main
+struct NutritionLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
